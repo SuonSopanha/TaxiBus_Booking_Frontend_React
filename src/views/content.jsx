23c39d050b63bf1a46e73eade116a3a218eb0ddf
Sopanha <129533258+SuonSopanha@users.noreply.github.com>
2024-03-14 22:19:30 +0700

    panha Mar14 #1

--- src/views/content.jsx
+++ src/views/content.jsx
@@ -5,12 +5,15 @@ import Login from "./pages/auth/login";
 import Signup from "./pages/auth/signup";
 import ForgetPassword from "./pages/auth/forgetPassword";
 import BusDetail from "./pages/bus/busDetail";
-
+import BookingForm from "./pages/booking/bookingForm";
+import BookingReciept from "./pages/booking/bookingReciept";
+import BookingRequest from "./pages/booking/bookingRequest";
+import TaxiProfile from "./pages/taxi/taxiProfile";
 
 const Content = () => {
   return (
     <>
-        <BusDetail/>
+        <TaxiProfile />
     </>
   );
 };
--- src/views/pages/booking/bookingRequest.jsx
+++ src/views/pages/booking/bookingRequest.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const BookingReciept = () => {
+const BookingRequest = () => {
   return (
     <div className="w-full">
       <div class="h-28"></div>
@@ -11,7 +11,7 @@ const BookingReciept = () => {
               <div class="pb-12">
                 <div class="flex w-full items-center justify-center rounded-full bg-yellow-300 p-2">
                   <p class="ml-2 text-2xl font-bold text-indigo-900">
-                    Booking Reciept
+                    Booking Request
                   </p>
                 </div>
 
@@ -169,123 +169,16 @@ const BookingReciept = () => {
                   <div class="flex w-full px-4 md:px-28">
                     <div class="flex w-52 py-8 font-medium">
                       <button class="relative mb-8 mt-5 flex h-12 w-40 items-center justify-center overflow-hidden rounded-lg bg-gradient-to-r from-yellow-500 to-amber-200 text-sm font-medium text-black shadow-lg shadow-slate-500 hover:border-2 hover:border-zinc-400 hover:shadow-none">
-                        <a href="homePage.html">Back</a>
+                        <a href="bookingReceipt.html">Accept Booking</a>
                         <div class="absolute right-0 mt-12 h-20 w-20 translate-x-1/2 transform rounded-full bg-yellow-200 bg-opacity-80"></div>
                       </button>
                     </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="relative">
-        <div class="h-20 w-full bg-sky-950"></div>
-        <div class="relative bg-sky-800">
-          <div class="mx-auto flex flex-col justify-center space-y-4 p-4 md:flex-row md:space-x-20 md:space-y-0">
-            <div class="relative -mt-24 rounded-lg bg-sky-900 text-white">
-              <div class="my-2 flex h-20 items-center justify-center">
-                <p class="text-4xl font-bold">TaxiBus Service</p>
-              </div>
-
-              <div class="m-4 pb-12">
-                <div>
-                  <p>
-                    Laverage agile framworks to provide a <br />
-                    robust synopsis for strategy collaborative <br />
-                    thinking for further the overall value <br />
-                    proposition.
-                  </p>
-                </div>
-                <div class="my-2 flex">
-                  <img
-                    width="48"
-                    height="48"
-                    src="https://img.icons8.com/color/48/circled-envelope.png"
-                    alt="circled-envelope"
-                  />
-                  <div class="ml-2">
-                    <p class="font-bold">Email</p>
-                    <p>TaxiBus777@gmial.com</p>
-                  </div>
-                </div>
-                <div class="my-2 flex">
-                  <img
-                    width="48"
-                    height="48"
-                    src="https://img.icons8.com/ios-filled/50/228BE6/apple-phone.png"
-                    alt="apple-phone"
-                  />
-                  <div class="ml-2">
-                    <p class="font-bold">Call Us</p>
-                    <p>010 23 1010 / 079 643 5050</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div class="flex items-center justify-center">
-              <div class="flex h-full items-center justify-center">
-                <div class="relative w-48 rounded-lg bg-sky-800 p-4 text-white">
-                  <div class="items-left flex flex-col space-y-4 font-medium">
-                    <p>
-                      <a href="#" class="hover:border-b-2">
-                        About Us
-                      </a>
-                    </p>
-                    <p>
-                      <a href="#" class="hover:border-b-2">
-                        Our Team
-                      </a>
-                    </p>
-                    <p>
-                      <a href="#" class="hover:border-b-2">
-                        Our Project
-                      </a>
-                    </p>
-                    <p>
-                      <a href="#" class="hover:border-b-2">
-                        Pricing
-                      </a>
-                    </p>
-                    <p>
-                      <a href="#" class="hover:border-b-2">
-                        Contact
-                      </a>
-                    </p>
-                  </div>
-                </div>
-              </div>
-
-              <div class="flex h-full items-center justify-center">
-                <div class="relative w-48 rounded-lg bg-sky-800 p-4 text-white">
-                  <div class="items-left flex flex-col space-y-4 font-medium">
-                    <p>
-                      <a href="#" class="hover:border-b-2">
-                        Style Guide
-                      </a>
-                    </p>
-                    <p>
-                      <a href="#" class="hover:border-b-2">
-                        Changelog
-                      </a>
-                    </p>
-                    <p>
-                      <a href="#" class="hover:border-b-2">
-                        License
-                      </a>
-                    </p>
-                    <p>
-                      <a href="#" class="hover:border-b-2">
-                        Protected
-                      </a>
-                    </p>
-                    <p>
-                      <a href="#" class="hover:border-b-2">
-                        Not Found
-                      </a>
-                    </p>
+                    <div class="flex w-52 py-8 font-medium">
+                      <button class="relative mb-8 mt-5 flex h-12 w-40 items-center justify-center overflow-hidden rounded-lg bg-gradient-to-r from-yellow-500 to-amber-200 text-sm font-medium text-black shadow-lg shadow-slate-500 hover:border-2 hover:border-zinc-400 hover:shadow-none">
+                        <a href="bookingForm.html">Decline Booking</a>
+                        <div class="absolute right-0 mt-12 h-20 w-20 translate-x-1/2 transform rounded-full bg-yellow-200 bg-opacity-80"></div>
+                      </button>
+                    </div>
                   </div>
                 </div>
               </div>
@@ -297,4 +190,4 @@ const BookingReciept = () => {
   );
 };
 
-export default BookingReciept;
+export default BookingRequest;
